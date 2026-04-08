6880695889f0a96b85f826ed6988cd1f9aae76e0
Vivek <b.vivek225432@gmail.com>
2026-04-08 17:02:46 -0400

    merge

--- test_calculator.py
+++ test_calculator.py
@@ -5,14 +5,20 @@
 import unittest
 from calculator import *
 
-class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+class TestCalculator(unittest.TestCase):
+    ######## Partner 2
+    def test_add(self):  # 3 assertions
+        assert add(-1, 1) == 0
+        assert add(-5, -5) == -10
+        assert add(1, 1) == 2
+
+    def test_subtract(self):  # 3 assertions
+        assert subtract(1, -1) == 2
+        assert subtract(1, 1) == 0
+        assert subtract(-5, -6) == 1
+
+    ##########################
 
     ######## Partner 1
     def test_multiply(self): # 3 assertions
@@ -28,44 +34,50 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):  # 1 assertion
+        # call division function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     div(0, 5)
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):  # 3 assertions
+        assert logarithm(25, 5) == 2
+        assert logarithm(8, 2) == 3
+        assert logarithm(1, 10) == 0
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+    def test_log_invalid_base(self):  # 1 assertion
+        # use same technique from test_divide_by_zero
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
+    ##########################
+
     ######## Partner 1
-    def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
+    def test_log_invalid_argument(self):  # 1 assertion
+            #     # call log function inside, example:
+            #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+            #     #     logarithm(0, 5)
         with self.assertRaises(ValueError):
             logarithm(0, 5)
 
-    def test_hypotenuse(self): # 3 assertions
+    def test_hypotenuse(self):  # 3 assertions
         assert hypotenuse(3, 4) == 5
         assert hypotenuse(2, 2) == square_root(8)
-        assert hypotenuse(30, 30 ) == square_root(1800)
+        assert hypotenuse(30, 30) == square_root(1800)
 
-    def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
+    def test_sqrt(self):  # 3 assertions
+            #     # Test for invalid argument, example:
+            #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
+            #     #    square_root(NUM)
+            #     # Test basic function
         assert square_root(25) == 5
         assert square_root(100) == 10
         with self.assertRaises(ValueError):
             square_root(-1)
-#     #    square_root(NUM)
+    #     #    square_root(NUM)
     ##########################
+    ##########################
+
 
 # Do not touch this
 if __name__ == "__main__":
